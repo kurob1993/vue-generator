bc400f673d1530c5ae28f690dd4f9889eedd008e
kurob1993 <kurob1993@gmail.com>
2020-06-17 15:57:29 +0700

    ukuran kolom ketika screen xtra small

--- resources/views/components/date.blade.php
+++ resources/views/components/date.blade.php
@@ -1,8 +1,8 @@
 <vs-row class="form-group">
-    <vs-col vs-w="4" vs-xs="12">
+    <vs-col vs-w="4" vs-xs="5">
         <label class="vs-input--label" for="{{$vmodel}}">{{$label}}</label>
     </vs-col>
-    <vs-col vs-w="8" vs-xs="12">
+    <vs-col vs-w="8" vs-xs="7">
         <ValidationProvider tag="div" rules="{{$required}}" name="{{$label}}" v-slot="{ errors }">
             <vs-input 
                 type="date" 
